aead492b5997f3aef8556b4c5bff1d8aed107107
Rohan Isaac <rohan_isaac@yahoo.com>
2018-05-29 22:55:15 -0400

    conda packages and matplotlib config files

---
+++ install_conda.sh
@@ -0,0 +1,6 @@
+# Install conda from anaconda.com
+
+conda update -y conda
+conda install -c conda-forge -y lmfit uncertainties pint peakutils
+conda install -y numpy pandas matplotlib scikit-learn jupyter notebook scipy
+conda update -y --all
